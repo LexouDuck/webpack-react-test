5d19bd28294de32000b1d580753dc56e0428e854
Lexou <lexou-dit-tuc@hotmail.fr>
2019-08-01 18:46:53 +0200

    fixed code style

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: React.FC = () =>
+{
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					<br/>Edit <code>src/App.tsx</code> and save to reload.
+					<br/>for real nigga ?
+					<br/>FOR REAL.
+					<br/>THIS IS WEBDEV.
+					<br/>EVERYTHING IS BEING TAKEN CARE OF.
+					<br/>THERE ARE NO WORRIES YOU SHOULD FILL YOUR HEAD WITH.
+					<br/><b>KEEP DEVELOPING</b>
+					<br/>
+					<br/><i>SEE YOU LATER, CODE MONKEY</i>
+				</p>
+				<a href="https://reactjs.org"
+					className="App-link"
+					target="_blank"
+					rel="noopener noreferrer">
+					Click Here to Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
