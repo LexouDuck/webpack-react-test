df7018549ef3515d144dce2dfce3ffc66cc23993
Lexou <lexou-dit-tuc@hotmail.fr>
2019-09-03 19:58:26 +0200

    menu works now, react is still dumb tho

--- src/menu.tsx
+++ src/menu.tsx
@@ -4,72 +4,100 @@ import {AppProps, AppState} from './main'
 
 
 
+interface Props_MenuItem_Number
+{
+	label:string,
+	value:number,
+	update:(value:number)=>void
+}
+interface State_MenuItem_Number
+{
+	value:number
+}
+class MenuItem_Number extends React.Component<Props_MenuItem_Number, State_MenuItem_Number>
+{
+	constructor(props:Props_MenuItem_Number, state:State_MenuItem_Number)
+	{
+		super(props, state);
+		this.state = { value: props.value };
+	}
+
+	handleChange_Number(event:React.ChangeEvent<HTMLInputElement>)
+	{
+		let new_value:number = Number(event.target.value);
+		this.setState({ value: new_value });
+		this.props.update(new_value);
+	}
+
+	render()
+	{
+		return (
+			<label>
+				{this.props.label}	<input type="number" value={this.state.value} onChange={this.handleChange_Number.bind(this)} />	<br/>
+			</label>
+		);
+	}
+}
+
+
+
 interface MenuProps
 {
 	default:AppState,
 	update:(state:AppState)=>void
 }
 
-export class Menu extends React.Component<MenuProps, AppState>
+class Menu extends React.Component<MenuProps>
 {
-	constructor(props:MenuProps, state:AppState)
+	state_menu:AppState;
+
+	constructor(props:MenuProps)
 	{
-		super(props, state);
-		this.state =
+		super(props);
+		this.state_menu =
 		{
 			min_x: props.default.min_x,
 			min_y: props.default.min_y,
 			max_x: props.default.max_x,
 			max_y: props.default.max_y,
 		};
-
-		this.handleChange_MinX = this.handleChange_MinX.bind(this);
-		this.handleChange_MinY = this.handleChange_MinY.bind(this);
-		this.handleChange_MaxX = this.handleChange_MaxX.bind(this);
-		this.handleChange_MaxY = this.handleChange_MaxY.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
-	componentDidUpdate()
+	handleChange_MinX(value:number)
 	{
-		this.render();
+		this.state_menu.min_x = value;
 	}
-
-	handleChange_MinX(event:React.ChangeEvent<HTMLInputElement>)
+	handleChange_MinY(value:number)
 	{
-		this.setState({min_x: Number(event.target.value)});
+		this.state_menu.min_y = value;
 	}
-	handleChange_MinY(event:React.ChangeEvent<HTMLInputElement>)
+	handleChange_MaxX(value:number)
 	{
-		this.setState({min_y: Number(event.target.value)});
+		this.state_menu.max_x = value;
 	}
-	handleChange_MaxX(event:React.ChangeEvent<HTMLInputElement>)
+	handleChange_MaxY(value:number)
 	{
-		this.setState({max_x: Number(event.target.value)});
-	}
-	handleChange_MaxY(event:React.ChangeEvent<HTMLInputElement>)
-	{
-		this.setState({max_y: Number(event.target.value)});
+		this.state_menu.max_y = value;
 	}
 
 	handleSubmit(event:React.FormEvent<HTMLFormElement>)
 	{
 		event.preventDefault();
-		this.props.update(this.state);
+		this.props.update(this.state_menu);
 	}
 
 	render()
 	{
 		return (
-			<form onSubmit={this.handleSubmit}>
-				<label>
-					Min X:	<input type="number" value={this.state.min_x} onChange={this.handleChange_MinX} />	<br/>
-					Min Y:	<input type="number" value={this.state.min_y} onChange={this.handleChange_MinY} />	<br/>
-					Max X:	<input type="number" value={this.state.max_x} onChange={this.handleChange_MaxX} />	<br/>
-					Max Y:	<input type="number" value={this.state.max_y} onChange={this.handleChange_MaxY} />	<br/>
-				</label>
-				<input type="submit" value="Submit" />
+			<form onSubmit={this.handleSubmit.bind(this)}>
+				<MenuItem_Number label={"Min X:"} value={this.props.default.min_x} update={this.handleChange_MinX.bind(this)} />
+				<MenuItem_Number label={"Min Y:"} value={this.props.default.min_y} update={this.handleChange_MinY.bind(this)} />
+				<MenuItem_Number label={"Max X:"} value={this.props.default.max_x} update={this.handleChange_MaxX.bind(this)} />
+				<MenuItem_Number label={"Max Y:"} value={this.props.default.max_y} update={this.handleChange_MaxY.bind(this)} />
+				<input type="submit" value="Refresh Fractal" />
 			</form>
 		);
 	}
 }
+
+export { Menu };
